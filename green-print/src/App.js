eb4688f55de9d946600759cfb07f2b1628efec63
Ishwant Singh <ishwant8@gmail.com>
2019-04-13 11:01:24 +0530

    added routes and updated app.js to show home component

--- green-print/src/App.js
+++ green-print/src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { connect } from "react-redux";
+// import { compose } from "redux";
+import styled from "styled-components";
+
 import "./App.css";
+
+import Headbar from "./components/layout/Headbar";
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Headbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
