9c54c5b75be2e7b2c3cb9252c15719e5b739c99e
Ishwant Singh <ishwant8@gmail.com>
2019-04-13 12:49:38 +0530

    made : About.js. updated: dashboard.js with info.

---
+++ green-print/src/components/layout/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <div>
+        Direct carbon emissions Direct carbon emissions come from sources that
+        are directly from the site that is producing a product.These emissions
+        can also be referred to as scope 1 and scope 2 emissions. Scope 1
+        emissions are emissions that are directly emitted from the site of the
+        process or service. An example for industry would be the emissions
+        related to burning a fuel on site. On the individual level, emissions
+        from personal vehicles or gas burning stoves would fall under scope 1.
+        Scope 2 emissions are the other emissions related to purchased
+        electricity, heat, and/or steam used on site .In the US, the EPA has
+        broken down electricity emission factors by state.
+      </div>
+      <div>
+        Indirect carbon emissions Indirect carbon emissions are emissions from
+        sources upstream or downstream from the process being studied, also
+        known as scope 3 emissions. Examples of upstream, indirect carbon
+        emissions may include: Transportation of materials/fuels Any energy used
+        outside of the production facility Wastes produced outside of the
+        production facility Examples of downstream, indirect carbon emissions
+        may include: Any end-of-life process or treatments Product and waste
+        transportation Emissions associated with selling the product
+      </div>
+    </div>
+  );
+}
