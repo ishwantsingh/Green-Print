8fcef700a1ec426ef085aaf816d2ecd0452a6724
Ishwant Singh <ishwant8@gmail.com>
2019-04-14 00:42:57 +0530

    testing branches

--- green-print/src/components/road/RoadHome.js
+++ green-print/src/components/road/RoadHome.js
@@ -6,6 +6,8 @@ const StyledContainer = styled.div`
   height: 100%;
   margin: 0 auto;
   .container {
+    height: 100vh;
+    width: 100vw;
     margin: 0 2%;
     .heading {
       font-size: 1.7rem;
@@ -16,31 +18,31 @@ const StyledContainer = styled.div`
 `;
 
 class RoadHome extends React.Component {
-  // mapboxgl = window.mapboxgl;
+  mapboxgl = window.mapboxgl;
+  MapboxDirections = window.MapboxDirections;
+  componentDidMount() {
+    this.mapboxgl.accessToken =
+      "pk.eyJ1IjoiaXNod2FudHNpbmdoIiwiYSI6ImNqdWUzZ2NjbjBndjA0ZW9od2t1aGpvNDIifQ.w1yxRYhbHY2Xw9aQ11R7zA";
 
+    let map = new this.mapboxgl.Map({
+      container: "rrr",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [77.041, 28.5917], // starting position
+      zoom: 16 // starting zoom
+    });
+    map.addControl(
+      new this.MapboxDirections({
+        accessToken: this.mapboxgl.accessToken
+      }),
+      "top-left"
+    );
+    map.addControl(new this.mapboxgl.NavigationControl());
+  }
   render() {
-    // this.mapboxgl.accessToken =
-    //   "pk.eyJ1IjoiaXNod2FudHNpbmdoIiwiYSI6ImNqdWUzZ2NjbjBndjA0ZW9od2t1aGpvNDIifQ.w1yxRYhbHY2Xw9aQ11R7zA";
-
-    // let map = new this.mapboxgl.Map({
-    //   container: "map",
-    //   style: "mapbox://styles/mapbox/streets-v11",
-    //   center: [77.041, 28.5917], // starting position
-    //   zoom: 16 // starting zoom
-    // });
-    // map.addControl(
-    //   new MapboxDirections({
-    //     accessToken: this.mapboxgl.accessToken
-    //   }),
-    //   "top-left"
-    // );
-    // map.addControl(new this.mapboxgl.NavigationControl());
     return (
       <StyledContainer>
-        <div className="container">
-          <input placeholder="Enter Start Point" />
-          <input placeholder="Enter End Point" />
-          {/* <div id="" map /> */}
+        <div className="container" id="rrr">
+          <div className="tesst" />
         </div>
       </StyledContainer>
     );
