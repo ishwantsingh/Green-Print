55442222209f79420fd19e331d6a83d1fb5ae0c6
Ishwant Singh <ishwant8@gmail.com>
2019-04-13 13:56:07 +0530

    feat: added road and metro links to AppHome

--- green-print/src/components/layout/AppHome.js
+++ green-print/src/components/layout/AppHome.js
@@ -1,5 +1,50 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  .container {
+    margin: 0 2%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .heading {
+      font-size: 1.7rem;
+      font-weight: bold;
+      text-align: center;
+    }
+    .button {
+      text-decoration: none;
+      font-size: 1.3em;
+      margin: 1em;
+      padding: 0.25em 1em;
+      border: 2px solid #009688;
+      border-radius: 3px;
+      color: #009688;
+    }
+    .links {
+      display: flex;
+      justify-content: center;
+    }
+  }
+`;
 export default function AppHome() {
-  return <div>App Home</div>;
+  return (
+    <StyledContainer>
+      <div className="container">
+        <div className="heading">Travelling Via ?</div>
+        <div className="links">
+          <Link to="/road" className="button">
+            Road
+          </Link>
+          <Link to="/metro" className="button">
+            Metro
+          </Link>
+        </div>
+      </div>
+    </StyledContainer>
+  );
 }
